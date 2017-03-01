4af6aa09616311e5d588956280fc4796ecdff9e7
Sprogis <sprogis1337@gmail.com>
2017-03-01 14:55:47 +0100

    fixat ingredient grejen

--- public/json/food.js
+++ public/json/food.js
@@ -1,42 +1,50 @@
+// var food = [
+//     {
+// 	"menuID" : 20,
+// 	"type"   : "food",
+// 	"label"  : "Hamburgare",
+// 	"price"  : 70,
+// 	"ingredients": [
+// 	    {
+// 		"onion"  : true,
+// 		"gluten" : true,
+// 		"vegan"  : false,
+// 		"lactose": true,
+// 		"pickles": true,
+// 		"cheese" : true,
+// 		"bacon"  : [
+// 		    {
+// 			"include" : false,
+// 			"price" : 5
+// 		    }
+// 		],
+// 		"egg"    : [
+// 		    {
+// 			"include" : false,
+// 			"price" : 5
+// 		    }
+// 		]
+// 	    }
+// 	]
+//     }
+// ]
+
+
 var food = [
-    {
-	"menuID" : 20,
-	"type"   : "food",
-	"label"  : "Hamburgare",
-	"price"  : 70,
-	"ingredients": [
-	    {
-		"onion"  : true,
-		"gluten" : true,
-		"vegan"  : false,
-		"lactose": true,
-		"pickles": true,
-		"cheese" : true,
-		"bacon"  : [
-		    {
-			"include" : false,
-			"price" : 5
-		    }
-		],
-		"egg"    : [
-		    {
-			"include" : false,
-			"price" : 5
-		    }
-		]
-	    }
-	]
-    }
-]
-
-
-var formalFood = {
     hamburgare = {
         "menuID" : 20,
         "type": "food",
-        "name": "Hamburger",
+        "label": "Hamburger",
         "price": 70,
-        "ingredients":["onion","cheese","pickles"],
-        "allergies": ["gluten free","lactose free"] 
-    }
-}
+        "ingredients":["onion","cheese","pickles","gluten","lactose"],
+	"removed":[],
+    },
+    ostburgare = {
+        "menuID" : 21,
+        "type": "food",
+        "label": "Ostburgare",
+        "price": 70,
+        "ingredients":["onion","cheese","pickles","gluten","lactose"],
+	"removed":[],
+    }   
+]
--- public/scripts/barToKitchen.js
+++ public/scripts/barToKitchen.js
@@ -6,6 +6,15 @@ var order = function(orderNum, tableNum, items) {
     this.state = 1; //0 - done, 1 - queue, 2 - in progress 
 }
 
+var foodi = function(id, label, price, removed) {
+    this.menuID = id;
+    this.type = food;
+    this.label = label;
+    this.price = price;
+    this.removed = removed;
+    this.special = false
+}
+
 new Vue ({
     el: '#mainID',
     mixins: [sharedVueData],
@@ -17,12 +26,34 @@ new Vue ({
 	total: 0
     },
     methods : {
-	
+
+	dropdown : function() {
+	        document.getElementById("myDropdown").classList.toggle("show");
+	},
 	sendItem : function(item) {
+	
 	    this.checkout.push(item);
 	    this.total += item.price;
 	    if(item.type === "food") {
-		this.toKitchen.push(item);
+
+		var checkboxes = document.getElementsByName(item.menuID);
+		for (var i=0; i < checkboxes.length; i++) {
+		    if(!checkboxes[i].checked) {
+			item.removed.push(checkboxes[i].value)
+		    }
+		}
+		var nitem = new foodi(item.menuID, item.label, item.price, item.removed);
+		if(nitem.removed.length != 0) {
+		    nitem.special = true;
+		}
+		this.toKitchen.push(nitem);
+		console.log(this.toKitchen.length)
+		for (var i=0; i < checkboxes.length; i++) {
+		    if(!checkboxes[i].checked) {
+			checkboxes[i].checked = "checked"
+		    }
+		}
+		item.removed = []
 	    }
 	},
 	recieveItem : function(item) {
@@ -45,6 +76,10 @@ new Vue ({
 		socket.emit('tableNr', tableNr);
 		var foobar = new order(orderNr, tableNr, this.toKitchen)
 		socket.emit('order', foobar);
+		this.toKitchen = [];
+		this.checkout = [];
+	    }
+	    else {
 		this.toKitchen = [];
 		this.checkout = [];
 	    }
